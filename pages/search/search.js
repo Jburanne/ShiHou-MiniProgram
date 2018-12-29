de5e65ae6db371059a24ebde48f8a8316b53370e
houwy <houwyvenny@126.com>
2018-12-29 23:41:10 +0800

    搜索界面排版完成，按字母排列城市

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,4 +1,5 @@
 // pages/search/search.js
+let staticData = require('../../data/staticData.js')
 var app = getApp()
 var hotCities = ["北京", "上海", "广州", "深圳", "南京", "武汉", "杭州"];
 
@@ -14,7 +15,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    
+    let cities = this.getSortedAreaObj(staticData.cities || [])
+    this.setData({
+      cities,
+      showItems: cities,
+    })   
   },
   choose(e) {
     console.log("点击了")
@@ -33,19 +38,32 @@ Page({
         page.onLoad();
       } 
     })
-    // let name = e.currentTarget.dataset.name
-    // let pages = getCurrentPages()
-    // let len = pages.length
-    // let indexPage = pages[len - 2]
-    // if (name) {
-    //   indexPage.search(name, () => {
-    //     wx.navigateBack({})
-    //   })
-    // } else {
-    //   indexPage.init({}, () => {
-    //     wx.navigateBack({})
-    //   })
-    // }
+  },
+
+  // 按照字母顺序生成需要的数据格式
+  getSortedAreaObj(areas) {
+    // let areas = staticData.areas
+    areas = areas.sort((a, b) => {
+      if (a.letter > b.letter) {
+        return 1
+      }
+      if (a.letter < b.letter) {
+        return -1
+      }
+      return 0
+    })
+    let obj = {}
+    for (let i = 0, len = areas.length; i < len; i++) {
+      let item = areas[i]
+      delete item.districts
+      let letter = item.letter
+      if (!obj[letter]) {
+        obj[letter] = []
+      }
+      obj[letter].push(item)
+    }
+    // 返回一个对象，直接用 wx:for 来遍历对象，index 为 key，item 为 value，item 是一个数组
+    return obj
   },
 
 
